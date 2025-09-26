3e7471d93b091ea2f311f3288e57a423c0599479
Su Ngo <quocsungo2000@gmail.com>
2025-09-26 14:38:59 +0700

    feat(train-rag): add endpoint ingest document to Qdrant

--- src/app/main.py
+++ src/app/main.py
@@ -6,6 +6,7 @@ from qdrant_client import QdrantClient
 from src.app.config.settings import settings
 from src.app.services.qdrant_collection import create_collection
 from src.app.api.call_bot import call_bot_router
+from src.app.api.rag import train_rag_router
 
 
 logger = logging.getLogger(__name__)
@@ -48,10 +49,10 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="Minimal RAG with FastAPI & Qdrant", version="1.0.0", lifespan=lifespan)
 
 app.include_router(call_bot_router, prefix="/api")
+app.include_router(train_rag_router, prefix="/api")
 
 
 def get_qdrant_client(request: Request) -> QdrantClient:
-    """Dependency để lấy QdrantClient đã khởi tạo."""
     return request.app.state.qdrant_client
 
 
